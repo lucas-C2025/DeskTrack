268acbee7613af003ac2fd9d0e1b518ea10d7108
Pedro-WB <128044342+Pedro-WB@users.noreply.github.com>
2025-05-22 09:59:42 -0230

    device.py

--- desktrack_project/backend/models/device.py
+++ desktrack_project/backend/models/device.py
@@ -1,1 +1,19 @@
 # Device model
+from sqlalchemy import Column, Integer, String, Enum
+from backend.database.connection import Base
+import enum
+
+class StatusEnum(str, enum.Enum):
+    ativo = 'ativo'
+    manutencao = 'manutenção'
+    desativado = 'desativado'
+
+class Device(Base):
+    __tablename__ = 'devices'
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, unique=True, index=True)
+    ip = Column(String)
+    sistema_operacional = Column(String)
+    usuario_responsavel = Column(String)
+    localizacao = Column(String)
+    status = Column(Enum(StatusEnum))
