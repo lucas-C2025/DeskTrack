8da172d4954e7bacf4fdd80e3640374d290c4608
Pedro-WB <128044342+Pedro-WB@users.noreply.github.com>
2025-05-22 10:00:12 -0230

    device.py

--- desktrack_project/backend/schemas/device.py
+++ desktrack_project/backend/schemas/device.py
@@ -1,1 +1,25 @@
 # Device schema
+from pydantic import BaseModel
+from typing import Optional
+from enum import Enum
+
+class StatusEnum(str, Enum):
+    ativo = 'ativo'
+    manutencao = 'manutenção'
+    desativado = 'desativado'
+
+class DeviceBase(BaseModel):
+    nome: str
+    ip: str
+    sistema_operacional: str
+    usuario_responsavel: str
+    localizacao: str
+    status: StatusEnum
+
+class DeviceCreate(DeviceBase):
+    pass
+
+class Device(DeviceBase):
+    id: int
+    class Config:
+        orm_mode = True
