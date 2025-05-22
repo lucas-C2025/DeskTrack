5795d3b7adc17876b87f4016db317e185bb21da7
Pedro-WB <128044342+Pedro-WB@users.noreply.github.com>
2025-05-22 09:56:37 -0230

    main.py

--- desktrack_project/backend/main.py
+++ desktrack_project/backend/main.py
@@ -1,1 +1,17 @@
 # FastAPI main app
+from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.requests import Request
+
+app = FastAPI()
+
+# Configurando Jinja2
+templates = Jinja2Templates(directory="frontend/templates")
+
+# Servindo arquivos estáticos
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+
+@app.get("/")
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "message": "Hello, FastAPI + Jinja2!"})
