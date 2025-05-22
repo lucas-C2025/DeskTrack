7daae2a13ab1df2ad87e7f10869224238a48c78a
Pedro-WB <128044342+Pedro-WB@users.noreply.github.com>
2025-05-22 09:59:01 -0230

    connection.py

--- desktrack_project/backend/database/connection.py
+++ desktrack_project/backend/database/connection.py
@@ -1,1 +1,9 @@
 # DB connection
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql://user:password@localhost/desktrack"  # ou mysql://...
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
