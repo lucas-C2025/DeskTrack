4ab89856678a23bbb7bfaea43fe38c2b89ae844d
Pedro-WB <128044342+Pedro-WB@users.noreply.github.com>
2025-05-22 09:56:15 -0230

    test_device.py

--- desktrack_project/tests/test_device.py
+++ desktrack_project/tests/test_device.py
@@ -1,1 +1,17 @@
 # Device tests
+from fastapi.testclient import TestClient
+from backend.main import app
+
+client = TestClient(app)
+
+def test_create_device():
+    response = client.post("/devices/", json={
+        "nome": "PC-LAB01",
+        "ip": "192.168.10.14",
+        "sistema_operacional": "Linux",
+        "usuario_responsavel": "João",
+        "localizacao": "Laboratório 3",
+        "status": "ativo"
+    })
+    assert response.status_code == 200
+    assert response.json()["nome"] == "PC-LAB01"
