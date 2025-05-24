940c2debabe71d9053d61148069f2267cbdc3945
Pedro-WB <128044342+Pedro-WB@users.noreply.github.com>
2025-05-24 09:26:53 -0300

    Delete desktrack_project directory

--- desktrack_project/README.md
+++
@@ -1,1 +0,0 @@
-# DeskTrack Project
--- desktrack_project/backend/reports/csv_export.py
+++
@@ -1,6 +0,0 @@
-# CSV export
-import pandas as pd
-
-def export_activities_to_csv(data, filename="activities.csv"):
-    df = pd.DataFrame(data)
-    df.to_csv(filename, index=False)
